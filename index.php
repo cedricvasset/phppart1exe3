fb3e123e971830c5540f1a55525dcbe34971175f
cedricvasset <vasset.cedric@orange.fr>
2018-12-14 16:02:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part1 ex4</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <p>
+    <?php
+    $km=1;
+    echo $km;
+    ?>
+  </p>
+  <p>
+    <?php
+    $km=3;
+    echo $km;
+    ?>
+  </p>
+  <p>
+    <?php
+    $km=125;
+    echo $km;
+    ?>
+  </p>
+</body>
+</html>
